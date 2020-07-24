5bba742a8fc898140555aeb10e67360d4407dedd
rony646 <rony646@users.noreply.github.com>
2020-07-24 12:34:40 -0300

    redux store created

--- redux-basics.js
+++ redux-basics.js
@@ -9,16 +9,37 @@ const initialState = {
 // Reducer
 
 const rootReducer = (state = initialState, action) => {
+    if(action.type === 'INC_COUNTER') {
+        return {
+            ...state,
+            counter: state.counter + 1 
+        };
+    } else if(action.type === 'ADD_COUNTER') {
+        return{
+            ...state,
+            counter: state.counter + action.value
+        }
+    }
     return state;
 }
 
 // Store
 
 const store = createStore(rootReducer);
-console.log(store.getState());
-
-// Dispatching Action
-
 
 
 // Subscription
+
+
+store.subscribe(() => {
+    console.log('[Subscribtion]', store.getState())
+});
+
+
+// Dispatching Action
+
+store.dispatch({type: 'INC_COUNTER'})
+store.dispatch({type: 'ADD_COUNTER', value: 20})
+
+
+
