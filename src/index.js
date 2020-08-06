8e64e1f28e15cec9b1c95530cd353e927364a5fd
rony646 <rony646@users.noreply.github.com>
2020-08-06 14:45:57 -0300

    middleware

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { createStore } from 'redux'
+import { createStore, applyMiddleware, compose } from 'redux'
 import { Provider } from 'react-redux'
 
 import './index.css';
@@ -8,7 +8,21 @@ import App from './App';
 import registerServiceWorker from './registerServiceWorker';
 import reducer from './store/reducer'
 
-const store = createStore(reducer);
+const logger = store => {
+    // Middleware
+    return next => {
+        return action => {
+            console.log('[MIDDLEWARE dispatching]:',  action);
+            const result = next(action);
+            console.log('[MIDDLEWARE] next state', store.getState());
+            return result
+        };
+    };
+};
+
+const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose; // Redux devtools
+
+const store = createStore(reducer, composeEnhancers(applyMiddleware(logger)));
 
 ReactDOM.render(<Provider store={store}><App /></Provider>, document.getElementById('root'));
 registerServiceWorker();
